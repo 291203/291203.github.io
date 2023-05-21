ff8b3ddd6a4c05b042325606c904a8536485629e
291203 <126445722+291203@users.noreply.github.com>
2023-05-21 16:27:17 +0300

    ashjxgjshax

--- lab22/lab22.js
+++ lab22/lab22.js
@@ -10,5 +10,10 @@
         return x**n;
     }
     function sumTo(n) {
-        return n+n;
+        let result = 0;
+        for (let i = 1; i <= n; i++) {
+        result += i;
+        }
+
+    return result;
     }
