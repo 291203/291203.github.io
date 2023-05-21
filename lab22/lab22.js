55c42f2d55180e8cb3a5dfd5a27f252584904a6a
291203 <126445722+291203@users.noreply.github.com>
2023-05-21 16:05:01 +0300

    kgk

--- lab22/lab22.js
+++ lab22/lab22.js
@@ -1,12 +1,12 @@
 function pow(x, n) {
     let result = x;
-    if(n>0){
+    if(n > 0){
         
         for (let i = 1; i < n; i++) {
         result *= x;
     }
 }
-else if(n = 0){
+else if(n == 0){
     result = 1;
 }
 else{
