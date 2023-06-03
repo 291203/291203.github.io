fcdc281e064b25ffbec0ff1356ecd16bae04e87a
291203 <126445722+291203@users.noreply.github.com>
2023-06-03 14:52:30 +0300

    fghh

--- lab22/lab22.js
+++ lab22/lab22.js
@@ -1,31 +1,25 @@
+function pow(x, n) {
+    return x ** n;
+}
 
-    'use strict';
-    /**
-     * 
-     * @param {number} x возводимое в степень число
-     * @param {number} n СтепенЬ - натуральное
-     * @return {number} - число x в стпени n
-     */
-    function pow(x, n) {
-        return x**n;
-    }
-    function sumTo(n) {
-        let result = 0;
-        for (let i = 1; i <= n; i++) {
-        result += i;
-        }
+function sumTo(n) {
+   let sum = 0;
+   for (let i = 1; i <= n; i++) {
+       sum += i
+   }
+   return sum;
+}
 
-    return result;
-    }
-    function factorial(n){
-        let result = BigInt(1);
-        for (let i = 1; i <= n;i++){
-            result *= BigInt(i);
-        }
-    return result;
-    }
-    function fib(n){
-        a = BigInt(1); 
+   
+function factorial(n) {   
+   if (n<=1) return 1n;
+   else {
+       res = BigInt (n) * BigInt(factorial(n-1))
+       return res;
+   }  
+}
+ function fib(n){
+   a = BigInt(1); 
    b=BigInt(1); 
    if (n== BigInt(0))
    {
@@ -39,22 +33,26 @@
            a=b;
            b=c;
        }
-       return b;}
-    }
-    function compare(x) {
-        let result = function (y) {    
-            if (y > x) return true
-            else if (y < x) return false
-            else return null;        
-        }
-        return result;
-      }
-      function sum() {
-        let result = 0;
-    
-        for (let i = 0; i < arguments.length; i++) {
-            result += arguments[i];
-        }
-    
-        return result;
-    }
+       return b;
+   }
+}
+
+function compare(x)
+{
+return function(y)
+{
+   if (y>x) return true;
+   else if (y<x) return false;
+   else if (y==x) return null;
+}
+}
+
+function sum()
+{
+   var result=0; 
+   for (var i=0; i<arguments.length; i++)
+   {
+       result+=arguments[i] 
+   }
+   return result;
+}
